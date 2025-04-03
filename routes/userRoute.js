4f4f1a15c4f0836a29957b01c7f13741fdb2ec1d
Riyan Uddin <107648620+riyanuddin17@users.noreply.github.com>
2025-04-04 03:22:32 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,6 +4,22 @@ import { create, deleteUser, fetch, update } from "../controller/userController.
 const route = express.Router();
 
 route.get("/getallusers", fetch)
+
+// or you can directly write as below, rather than wirting route in one file and API in controller.js
+
+// route.get("/getallusers", async (req, res) => {
+//     try {
+//         const users = await User.find();
+//         if (users.length === 0) {
+//             return res.status(404).json({ message: "User not Found." });
+//         }
+//         res.status(200).json(users);
+//     } catch (error) {
+//         res.status(500).json({ error: "Internal Server Error." });
+//     }
+// });
+
+
 route.post ("/create",create)
 route.put("/update/:id", update)
 route.delete("/delete/:id",deleteUser)
